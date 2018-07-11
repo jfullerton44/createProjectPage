718e6889b5b1c427d87705fa8bfcbd65798e01c2
George Magloire <gmagloire22@gmail.com>
2018-07-11 10:57:14 +0200

    added section to put claim schema and form into project json

--- src/dash-board.jsx
+++ src/dash-board.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Ixo } from 'ixo-module';
 import Launchbutton from './launch-button';
 import Web3 from 'web3';
-import { Redirect } from 'react-router-dom';
 import swal from 'sweetalert';
 
 
@@ -24,11 +23,9 @@ export default class Dashboard extends React.Component {
     this.handleMessageBodyChanged2 = this.handleMessageBodyChanged2.bind(this);
     this.handleMessageBodyChanged3 = this.handleMessageBodyChanged3.bind(this);
     this.getEthereumAddressAsync = this.getEthereumAddressAsync.bind(this);
-    this.handleRequestInfoButtonClicked = this.handleRequestInfoButtonClicked.bind(this);
+    this.handleRequestInfoButtonClicked = this.handleRequestInfoButtonClicked.bind(this)
     this.handleFormButtonClicked = this.handleFormButtonClicked.bind(this);
     this.handleSchemaButtonClicked = this.handleSchemaButtonClicked.bind(this);
-
-
     if (this.blockchainProviders.metamask.doShow) {
       this.initProvider(this.blockchainProviders.metamask);
     }
@@ -38,6 +35,7 @@ export default class Dashboard extends React.Component {
 
     this.signMessageWithProvider = this.signMessageWithProvider.bind(this);
     this.signData = this.signData.bind(this);
+
   }
 
   componentDidMount() {
@@ -137,7 +135,6 @@ export default class Dashboard extends React.Component {
     const blockchainProvider = (providerId === this.blockchainProviders.metamask.id) ? this.blockchainProviders.metamask : this.blockchainProviders.ixo_keysafe;
     this.signMessageWithProvider(this.state.messageBody, blockchainProvider, "http://localhost:5000/");
   }
-
   handleSchemaButtonClicked(providerId) {
     if (this.state.messageBody2.length === 0) {
       return;
@@ -174,113 +171,137 @@ export default class Dashboard extends React.Component {
     }
   }
 
+  // bit64 encoding function; "text" parameter is a json string
+  encode(text) {
+    var utf8 = require('utf8');
+    var binaryToBase64 = require('binaryToBase64');
+    var bytes = utf8.encode(text);
+    var encoded = binaryToBase64(bytes);
+    return encoded;
+  }
+
+  // upload documents to pds
+  uploadDoc(text, PDSURL) {
+    let dataUrl = 'data:application/json;base64, ' + encode(text);
+    result = ixo.project.createPublic(dataUrl, PDSURL);
+    console.log('Document hash: ' + result);
+  }
 
 
-signMessageWithProvider(message, blockchainProvider, PDSURL) {
-  //JSON.stringify(message)
-  if (blockchainProvider.id === this.blockchainProviders.ixo_keysafe.id) {
-    this.blockchainProviders.ixo_keysafe.provider.requestSigning(message, (error, response) => {
-      //alert(`Dashboard handling received response for SIGN response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`)
-      console.log(`Dashboard handling received response for SIGN response: \n${JSON.stringify(response)}\n, error: \n${JSON.stringify(error)}\n`)
-      try {
-        this.state.ixo.project.createProject(JSON.parse(message), response, PDSURL).then((result) => {
-          console.log(`Project Details:   \n${JSON.stringify(result)}`)
-          swal({
-            title: 'Your project has been created!',
-            text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
-            type: "success"
+
+  // have user sign and upload their project
+  signMessageWithProvider(message, blockchainProvider, PDSURL) {
+    // encode the claim schema and claim form json strings and upload them to pds
+    var encodedClaimSchema = encode(claimSchemaJSON);
+    var encodedClaimForm = encode(claimFormJSON);
+    var schemaHash = uploadDoc(encodedClaimSchema, PDSURL);
+    var formHash = uploadDoc(encodedClaimForm, PDSURL);
+
+    // insert hashes into project.json
+    message["templates"]["claim"]["schema"] = schemaHash;
+    message["templates"]["claim"]["form"] = formHash;
+    if (blockchainProvider.id === this.blockchainProviders.ixo_keysafe.id) {
+      this.blockchainProviders.ixo_keysafe.provider.requestSigning(message, (error, response) => {
+        //alert(`Dashboard handling received response for SIGN response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`)
+        console.log(`Dashboard handling received response for SIGN response: \n${JSON.stringify(response)}\n, error: \n${JSON.stringify(error)}\n`)
+        try {
+          this.state.ixo.project.createProject(JSON.parse(message), response, PDSURL).then((result) => {
+            console.log(`Project Details:   \n${JSON.stringify(result)}`)
+            swal({
+              title: 'Your project has been created!',
+              text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
+              type: "success"
+            })
+              .then(redirect => {
+                if (redirect) {
+                  window.location.href = 'https://ixo.foundation/';
+                }
+              });
           })
-            .then(redirect => {
-              if (redirect) {
-                window.location.href = 'https://ixo.foundation/';
-              }
-            });
-        })
-      } catch (error) {
-        console.log("Incorrect PDS URL format")
-        swal("ERROR", "Incorrect PDS URL format", "error")
-      }
+        } catch (error) {
+          console.log("Incorrect PDS URL format")
+          swal("ERROR", "Incorrect PDS URL format", "error")
+        }
 
-    })
-    return
-  } else {
-    this.getEthereumAddressAsync().then(address => {
-      console.log(`${blockchainProvider.extension} -> Address: ${address}`);
+      })
+      return
+    } else {
+      this.getEthereumAddressAsync().then(address => {
+        console.log(`${blockchainProvider.extension} -> Address: ${address}`);
 
-      // actual signing ->>
-      var dataInHex = '0x' + new Buffer(message).toString('hex');
+        // actual signing ->>
+        var dataInHex = '0x' + new Buffer(message).toString('hex');
 
-      blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
-        .then(console.log);
+        blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
+          .then(console.log);
+      });
+    }
+  }
+
+  getEthereumAddressAsync() {
+    const eth = this.blockchainProviders.metamask.provider.eth;
+    return new Promise((resolve, reject) => {
+      // resolve(provider.debug());
+      eth.getAccounts(function (error, accounts) {
+        if (error || 0 === accounts.length) {
+          reject(error);
+        }
+        resolve(accounts[0]);
+      });
     });
   }
-}
 
-getEthereumAddressAsync() {
-  const eth = this.blockchainProviders.metamask.provider.eth;
-  return new Promise((resolve, reject) => {
-    // resolve(provider.debug());
-    eth.getAccounts(function (error, accounts) {
-      if (error || 0 === accounts.length) {
-        reject(error);
-      }
-      resolve(accounts[0]);
-    });
-  });
-}
 
-render() {
-  return (
-    <div>
-      {/* {this.blockchainProviders.ixo_keysafe.doShow && 
+  render() {
+    return (
+      <div>
+        {/* {this.blockchainProviders.ixo_keysafe.doShow && 
           <button onClick={this.handleSimulateDidDocLedgeringButtonClicked}>Ledger DID Manually</button>
         }
         {this.blockchainProviders.ixo_keysafe.doShow && 
           <button onClick={this.handleRequestInfoButtonClicked}>ixo INFO</button>
         } */}
-      {/* <div></div>
+        {/* <div></div>
         <br></br>
         <br></br>
         <br></br> */}
-
-      <br></br>
-      <input value={this.state.messageBody2} onChange={this.handleMessageBodyChanged2} />
-      <Launchbutton
-        provider={this.blockchainProviders.ixo_keysafe.id}
-        title="Sign Schema"
-        handleLaunchEvent={this.handleSchemaButtonClicked} />
+        <br></br>
+        <input value={this.state.messageBody2} onChange={this.handleMessageBodyChanged2} />
+        <Launchbutton
+          provider={this.blockchainProviders.ixo_keysafe.id}
+          title="Sign Schema"
+          handleLaunchEvent={this.handleSchemaButtonClicked} />
         <br></br>
         Paste the Schema into the above textbox to Sign
 
       <br></br>
-      <br></br>
-      <input value={this.state.messageBody3} onChange={this.handleMessageBodyChanged3} />
-      <Launchbutton
-        provider={this.blockchainProviders.ixo_keysafe.id}
-        title="Sign Form"
-        handleLaunchEvent={this.handleFormButtonClicked} />
         <br></br>
-        Paste the Form into the above textbox to Sign
-      <br></br>
-      <br></br>
-      <br></br>
-      <input value={this.state.messageBody} onChange={this.handleMessageBodyChanged} />
-      {this.blockchainProviders.ixo_keysafe.doShow &&
+        <input value={this.state.messageBody3} onChange={this.handleMessageBodyChanged3} />
         <Launchbutton
           provider={this.blockchainProviders.ixo_keysafe.id}
-          title="ixo Sign and Create"
-          handleLaunchEvent={this.handleExtensionLaunch} />
+          title="Sign Form"
+          handleLaunchEvent={this.handleFormButtonClicked} />
+        <br></br>
+        Paste the Form into the above textbox to Sign
+       <br></br>
+        <br></br>
+        <br></br>
+        <input value={this.state.messageBody} onChange={this.handleMessageBodyChanged} />
+        {this.blockchainProviders.ixo_keysafe.doShow &&
+          <Launchbutton
+            provider={this.blockchainProviders.ixo_keysafe.id}
+            title="ixo Sign and Create"
+            handleLaunchEvent={this.handleExtensionLaunch} />
+        }
 
-      }
 
-
-      {this.blockchainProviders.metamask.doShow &&
-        <Launchbutton
-          provider={this.blockchainProviders.metamask.id}
-          title="Metamask Sign"
-          handleLaunchEvent={this.handleExtensionLaunch} />
-      }
-    </div>
-  )
-}
+        {this.blockchainProviders.metamask.doShow &&
+          <Launchbutton
+            provider={this.blockchainProviders.metamask.id}
+            title="Metamask Sign"
+            handleLaunchEvent={this.handleExtensionLaunch} />
+        }
+      </div>
+    )
+  }
 }
