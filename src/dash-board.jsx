987e319191ef51fac92791c1d633e46b8aaf2783
George Magloire <gmagloire22@gmail.com>
2018-07-12 15:13:11 +0200

    changed some things

--- src/dash-board.jsx
+++ src/dash-board.jsx
@@ -41,6 +41,7 @@ export default class Dashboard extends React.Component {
 
     this.signMessageWithProvider = this.signMessageWithProvider.bind(this);
     this.signData = this.signData.bind(this);
+    this.uploadAndInsert = this.uploadAndInsert.bind(this);
 
   }
 
@@ -224,8 +225,10 @@ export default class Dashboard extends React.Component {
   //   });
   // }
 
-  uploadAndInsert(schemaText, formText, PDSURL, message, type) {
-    let dataUrl1 = 'data:application/json;base64, ' + schemaText;
+  uploadAndInsert(PDSURL, message, blockchainProvider) {
+    var encodedClaimSchema = this.encodeJSON(this.state.messageBody2);
+    console.log("Encoded schema json: " + encodedClaimSchema);
+    let dataUrl1 = 'data:application/json;base64, ' + encodedClaimSchema;
     var hash = this.state.ixo.project.createPublic(dataUrl1, PDSURL).then((result) => {
       // check output, it should now output the hash   
       console.log("Result: " + JSON.stringify(result));
@@ -239,7 +242,9 @@ export default class Dashboard extends React.Component {
       projectJSON['templates']['schema'] = JSON.stringify(result.result);
 
 
-      let dataUrl2 = 'data:application/json;base64, ' + formText;
+      var encodedClaimForm = this.encodeJSON(this.state.messageBody3);
+      console.log("Encoded form json: " + encodedClaimForm);
+      let dataUrl2 = 'data:application/json;base64, ' + encodedClaimForm;
       var hash2 = this.state.ixo.project.createPublic(dataUrl1, PDSURL).then((result) => {
         // check output, it should now output the hash   
         console.log("Result: " + JSON.stringify(result));
@@ -249,16 +254,48 @@ export default class Dashboard extends React.Component {
 
         // check to see if templates were inserted into project json
         console.log("Project JSON templates section after additions: " + JSON.stringify(projectJSON['templates']));
-        
-        // update the state of globProjJSON to match 
-        this.state.globProjJSON = projectJSON;
-        console.log("global projectJSON: " + JSON.stringifythis.state.globProjJSON['templates']['form']);
 
       }).catch((error) => {
         console.log("Error, unable to return form");
         console.log(error);
       });
 
+      if (blockchainProvider.id === this.blockchainProviders.ixo_keysafe.id) {
+        this.blockchainProviders.ixo_keysafe.provider.requestSigning(message, (error, response) => {
+          //alert(`Dashboard handling received response for SIGN response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`)
+          console.log(`Dashboard handling received response for SIGN response: \n${JSON.stringify(response)}\n, error: \n${JSON.stringify(error)}\n`)
+          try {
+            this.state.ixo.project.createProject(JSON.parse(message), response, PDSURL).then((result) => {
+              console.log(`Project Details:   \n${JSON.stringify(result)}`)
+              swal({
+                title: 'Your project has been created!',
+                text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
+                type: "success"
+              })
+                .then(redirect => {
+                  if (redirect) {
+                    window.location.href = 'https://ixo.foundation/';
+                  }
+                });
+            })
+          } catch (error) {
+            console.log("Incorrect PDS URL format")
+            swal("ERROR", "Incorrect PDS URL format", "error")
+          }
+
+        })
+        return
+      } else {
+        this.getEthereumAddressAsync().then(address => {
+          console.log(`${blockchainProvider.extension} -> Address: ${address}`);
+
+          // actual signing ->>
+          var dataInHex = '0x' + new Buffer(message).toString('hex');
+
+          blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
+            .then(console.log);
+        });
+      }
     }).catch((error) => {
       console.log("Error, unable to return schema hash");
       console.log(error);
@@ -266,52 +303,52 @@ export default class Dashboard extends React.Component {
   }
 
   // have user sign and upload their project
-  signMessageWithProvider(message, blockchainProvider, PDSURL) {
-    // encode the claim schema and claim form json strings and upload them to pds
-    var encodedClaimSchema = this.encodeJSON(this.state.messageBody2);
-    console.log("Encoded schema json: " + encodedClaimSchema);
-    var encodedClaimForm = this.encodeJSON(this.state.messageBody3);
-    console.log("Encoded form json: " + encodedClaimForm);
-    var hashes = this.uploadAndInsert(encodedClaimSchema, encodedClaimForm, PDSURL, this.state.messageBody, "schema");
+  // signMessageWithProvider(message, blockchainProvider, PDSURL) {
+  //   // encode the claim schema and claim form json strings and upload them to pds
+  //   var encodedClaimSchema = this.encodeJSON(this.state.messageBody2);
+  //   console.log("Encoded schema json: " + encodedClaimSchema);
+  //   var encodedClaimForm = this.encodeJSON(this.state.messageBody3);
+  //   console.log("Encoded form json: " + encodedClaimForm);
+  //   var hashes = this.uploadAndInsert(encodedClaimSchema, encodedClaimForm, PDSURL, this.state.messageBody, "schema");
 
 
-    if (blockchainProvider.id === this.blockchainProviders.ixo_keysafe.id) {
-      this.blockchainProviders.ixo_keysafe.provider.requestSigning(message, (error, response) => {
-        //alert(`Dashboard handling received response for SIGN response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`)
-        console.log(`Dashboard handling received response for SIGN response: \n${JSON.stringify(response)}\n, error: \n${JSON.stringify(error)}\n`)
-        try {
-          this.state.ixo.project.createProject(JSON.parse(message), response, PDSURL).then((result) => {
-            console.log(`Project Details:   \n${JSON.stringify(result)}`)
-            swal({
-              title: 'Your project has been created!',
-              text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
-              type: "success"
-            })
-              .then(redirect => {
-                if (redirect) {
-                  window.location.href = 'https://ixo.foundation/';
-                }
-              });
-          })
-        } catch (error) {
-          console.log("Incorrect PDS URL format")
-          swal("ERROR", "Incorrect PDS URL format", "error")
-        }
+  //   if (blockchainProvider.id === this.blockchainProviders.ixo_keysafe.id) {
+  //     this.blockchainProviders.ixo_keysafe.provider.requestSigning(message, (error, response) => {
+  //       //alert(`Dashboard handling received response for SIGN response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`)
+  //       console.log(`Dashboard handling received response for SIGN response: \n${JSON.stringify(response)}\n, error: \n${JSON.stringify(error)}\n`)
+  //       try {
+  //         this.state.ixo.project.createProject(JSON.parse(message), response, PDSURL).then((result) => {
+  //           console.log(`Project Details:   \n${JSON.stringify(result)}`)
+  //           swal({
+  //             title: 'Your project has been created!',
+  //             text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
+  //             type: "success"
+  //           })
+  //             .then(redirect => {
+  //               if (redirect) {
+  //                 window.location.href = 'https://ixo.foundation/';
+  //               }
+  //             });
+  //         })
+  //       } catch (error) {
+  //         console.log("Incorrect PDS URL format")
+  //         swal("ERROR", "Incorrect PDS URL format", "error")
+  //       }
 
-      })
-      return
-    } else {
-      this.getEthereumAddressAsync().then(address => {
-        console.log(`${blockchainProvider.extension} -> Address: ${address}`);
+  //     })
+  //     return
+  //   } else {
+  //     this.getEthereumAddressAsync().then(address => {
+  //       console.log(`${blockchainProvider.extension} -> Address: ${address}`);
 
-        // actual signing ->>
-        var dataInHex = '0x' + new Buffer(message).toString('hex');
+  //       // actual signing ->>
+  //       var dataInHex = '0x' + new Buffer(message).toString('hex');
 
-        blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
-          .then(console.log);
-      });
-    }
-  }
+  //       blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
+  //         .then(console.log);
+  //     });
+  //   }
+  // }
 
   getEthereumAddressAsync() {
     const eth = this.blockchainProviders.metamask.provider.eth;
@@ -342,20 +379,20 @@ export default class Dashboard extends React.Component {
         <br></br> */}
         <br></br>
         <input value={this.state.messageBody2} onChange={this.handleMessageBodyChanged2} />
-        {/* <Launchbutton
+        <Launchbutton
           provider={this.blockchainProviders.ixo_keysafe.id}
           title="Sign Schema"
-          handleLaunchEvent={this.handleSchemaButtonClicked} /> */}
+          handleLaunchEvent={this.handleSchemaButtonClicked} />
         <br></br>
         Paste the Schema into the above textbox to Sign
 
       <br></br>
         <br></br>
         <input value={this.state.messageBody3} onChange={this.handleMessageBodyChanged3} />
-        {/* <Launchbutton
+        <Launchbutton
           provider={this.blockchainProviders.ixo_keysafe.id}
           title="Sign Form"
-          handleLaunchEvent={this.handleFormButtonClicked} /> */}
+          handleLaunchEvent={this.handleFormButtonClicked} />
         <br></br>
         Paste the Form into the above textbox to Sign
        <br></br>
